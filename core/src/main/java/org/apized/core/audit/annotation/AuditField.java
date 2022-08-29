17824b5d66e3430040979ff6dced21c2f15de101
Luís Serralheiro <luis.serralheiro@websummit.com>
2022-08-29 12:48:08 +0200

    Fixing the targets of the audit & event annotations

--- core/src/main/java/org/apized/core/audit/annotation/AuditField.java
+++ core/src/main/java/org/apized/core/audit/annotation/AuditField.java
@@ -22,7 +22,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface AuditField {
   String[] value() default {};
 }
