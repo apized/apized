10a89a17a4ade7b5caa4abf0c5bf1a50534b4f1b
Luís Serralheiro <encavadas@gmail.com>
2023-10-03 18:32:56 +0200

    Fixing injection issue detected on native builds

--- micronaut-core/src/main/java/org/apized/micronaut/server/mvc/MicronautModelService.java
+++ micronaut-core/src/main/java/org/apized/micronaut/server/mvc/MicronautModelService.java
@@ -2,15 +2,17 @@ package org.apized.micronaut.server.mvc;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.type.Argument;
-import jakarta.inject.Inject;
 import org.apized.core.model.Model;
 import org.apized.core.mvc.AbstractModelService;
 
 import java.util.Optional;
 
 public abstract class MicronautModelService<T extends Model> extends AbstractModelService<T> {
-  @Inject
-  ApplicationContext appContext;
+  protected ApplicationContext appContext;
+
+  public MicronautModelService(ApplicationContext appContext) {
+    this.appContext = appContext;
+  }
 
   @Override
   public <K> Optional<K> findBean(Argument<K> argument) {
